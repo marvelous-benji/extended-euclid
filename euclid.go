cd6725433fea6b746313477d94d90c988dec2ab7
marvelous-benji <61952411+marvelous-benji@users.noreply.github.com>
2021-09-01 12:13:38 +0100

    Update euclid.go

--- euclid.go
+++ euclid.go
@@ -3,6 +3,23 @@ package main
 
 import "fmt"
 
+/*
+Insight comes from the Bezout lemma:
+For nonzero integers a and b, let d be the greatest common divisor d=gcd⁡(a,b)
+Then, there exist integers x and y such that
+		ax+by=d
+a special case is when a and b are coprime (that is relatively prime)
+then d = gcd(a,b) = 1 implies ax + by = 1
+given a natural number n, if ax = 1 mod n, then we say x is the multiplicative
+inverse of 'a' modulo n or the modular multiplicative of 'a' in n.
+A necessary and sufficient condition for 'a' to have a multiplicative inverse
+in n is that gcd(a,n) = 1. Thus there exist integers x and y such that
+ax + yn = 1 ==> ax - 1 = (-y)n.
+Using the extended Euclidean algorithm we can get the bezout coefficients
+and this is implemented below
+*/
+
+
 
 
 
